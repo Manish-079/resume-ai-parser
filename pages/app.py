13e5eccc1a6463a5ae5d36c9191c04e3a8df3abd
Manish-079 <manish.jaggoe@gmail.com>
2026-03-26 18:49:12 +0100

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -76,12 +76,12 @@ if "analysis_prompt_input" not in st.session_state:
 # =========================================================
 # OPENAI
 # =========================================================
-api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-if not api_key:
-    api_key = st.secrets.get("OPENAI_API_KEY", "")
+if not OPENAI_API_KEY:
+    OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", "")
 
-client = OpenAI(api_key=api_key) if api_key else None
+client = OpenAI(api_key=OPENAI_API_KEY) if OPENAI_API_KEY else None
 
 # =========================================================
 # DATABASE
